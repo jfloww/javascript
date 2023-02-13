fb22242f4c7ac0affceaf3eabc9d19e27dd0ad14
Jay (Jaehoon) Jung <113634739+JayJung98@users.noreply.github.com>
2023-02-13 14:47:17 -0600

    Add files via upload

---
+++ 10.async.js
@@ -0,0 +1,64 @@
+// async & await
+// clear style of using promise 
+
+// 1. async
+async function fetchUser() {
+    return 'jay'
+}
+// ==
+// function fetchUser() {
+//     return new Promise((resolve, reject) => {
+//         // do network request in 10 secs ...
+//         resolve('jay');
+//     });
+// }
+
+const user = fetchUser();
+user.then(console.log);
+console.log(user);
+
+// 2. await
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getApple() {
+    await delay(2000);
+    return 'apple';
+}
+
+async function getBanana() {
+    await delay(1000);
+    return 'banana';
+}
+
+
+// EX:) 
+// function pickFruits() {
+//     return getApple()
+//     .then(apple => {
+//         return getBanana()
+//         .then(banana => `${apple} + ${banana}`);
+//     })
+// }
+// --> Simple
+async function pickFruits() {
+    const applePromise = getApple();
+    const bananaPromise = getBanana();
+    const apple = await applePromise;
+    const banana = await bananaPromise;
+    return `${apple} + ${banana}`;
+}
+pickFruits().then(console.log);
+
+// 3. useful Promise APIs
+function pickAllFruits() {
+    return Promise.all([getApple(), getBanana()])
+    .then(fruits => fruits.join(' ++ '));
+}
+pickAllFruits().then(console.log);
+
+function pickOnlyOne() {
+    return Promise.race([getApple(), getBanana()]);
+}
+pickOnlyOne().then(console.log);
